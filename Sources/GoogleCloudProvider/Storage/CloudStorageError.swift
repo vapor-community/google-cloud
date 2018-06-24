bb539fa440a0517ff4be1dacfa027623e22f1e88
Andrew Edwards <andrewangeta@gmail.com>
2018-06-24 09:48:41 -0400

    Added proper error handling.

--- Sources/GoogleCloudProvider/Storage/CloudStorageError.swift
+++ Sources/GoogleCloudProvider/Storage/CloudStorageError.swift
@@ -7,36 +7,29 @@
 
 import Vapor
 
-public enum CloudStorageError: Error {
-    
-    public struct CloudStorageErrorBody: Content {
-        
-    }
-    
-    case found
-    case seeOther
-    case notModified
-    case temporaryRedirect
-    case resumeIncomplete
-    case badRequest
-    case unauthorized
-    case forbidden
-    case notFound
-    case methodNotAllowed
-    case conflict
-    case gone
-    case lengthRequired
-    case preconditionFailed
-    case payloadTooLarge
-    case requestedRangeNotSatisfiable
-}
-
-extension CloudStorageError: Debuggable {
+// https://cloud.google.com/storage/docs/json_api/v1/status-codes
+public struct CloudStorageError: GoogleCloudModel, Error, Debuggable {
     public var identifier: String {
-        return ""
+        return "\(self.error.code)\(self.error.message)"
     }
     
     public var reason: String {
-        return ""
+        return self.error.message
     }
+    
+    public var error: CloudStorageErrorBody
+}
+
+public struct CloudStorageErrorBody: Content {
+    public var errors: [CloudStorageErrors]
+    public var code: Int
+    public var message: String
+}
+
+public struct CloudStorageErrors: Content {
+    public var domain: String?
+    public var reason: String?
+    public var message: String?
+    public var locationType: String?
+    public var location: String?
 }
